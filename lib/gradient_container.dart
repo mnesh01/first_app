3a4c48c2357aa9526eabf593211622ad755627fa
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2024-11-05 12:07:25 +0300

    create gradient_container

---
+++ lib/gradient_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color.fromARGB(255, 154, 170, 13), Colors.teal],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: (Text(
+          'MIAMI',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+          ),
+        )),
+      ),
+    );
+  }
+}
