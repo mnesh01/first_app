981ff4449c7aea36d8f696e4b00753c2da92022b
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2024-11-05 14:43:11 +0300

    Add decoration alignment

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,16 +1,19 @@
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Color.fromARGB(255, 154, 170, 13), Colors.teal],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: const [Color.fromARGB(255, 154, 170, 13), Colors.teal],
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(child: StyledText()),
