ef8639924cb1cd5b4183a534f757f8293c1fc5d5
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2024-11-06 18:19:59 +0300

    Minor styling

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -7,10 +7,7 @@ var endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
 
-  void rollDice() {
-    
-
-  }
+  void rollDice() {}
 
   final List<Color> colors;
   @override
@@ -25,12 +22,24 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
           child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           Image.asset(
             'assets/images/dice-1.png',
             width: 100,
           ),
-          TextButton(onPressed: rollDice, child: const Text('Roll dice'))
+          TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              child: const Text('Roll dice'))
         ],
       )),
     );
